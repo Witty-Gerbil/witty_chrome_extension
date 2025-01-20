397a6449a437123b65f357e803c326369864171a
PhantomOPBro <30814769+phantomOPBro@users.noreply.github.com>
2025-01-19 20:45:45 -0500

    Update start_server.py

--- witty_gerbil_backend_api/start_server.py
+++ witty_gerbil_backend_api/start_server.py
@@ -3,8 +3,14 @@ import os
 from dotenv import load_dotenv
 import uvicorn
 
-# Load environment variables from .env file
-load_dotenv()
+#grab the parent directory path where the .env file is
+parent_dir = os.path.abspath(os.path.join(os.getcwd(), ".."))
+
+# Specify the path to the .env file in the parent directory
+env_path = os.path.join(parent_dir, ".env")
+
+# Load the .env file from the specified path
+load_dotenv(env_path)
 
 HOST = os.getenv("HOST", "0.0.0.0")
 PORT = int(os.getenv("PORT", 8000))
